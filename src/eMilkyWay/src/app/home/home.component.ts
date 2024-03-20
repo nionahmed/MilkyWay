2733d672ad2ea61dc0b26e7dbd95cf3067f8f232
Md Nurul Amin <nurulaminnion@gmail.com>
2024-03-21 00:46:59 +0600

    carousel slider done

--- src/eMilkyWay/src/app/home/home.component.ts
+++ src/eMilkyWay/src/app/home/home.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
+import { NgFor, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule, NgFor, UpperCasePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  popularProduct:undefined|product[];
+  //images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    this.productService.popularProducts().subscribe((data)=>{
+      this.popularProduct=data;
+    })
+  }
 }
